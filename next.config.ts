357124578f2aed6f3e7e110c7a5a9ef97a16ed1f
Germán Gabriel <germangabriel1998@gmail.com>
2025-05-29 11:28:05 -0300

    Image optimization OFF

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    unoptimized: true, // disables next/image optimization globally
+  },
 };
 
 export default nextConfig;
